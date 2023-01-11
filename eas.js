5d8da459d2c47acce254608b863b20c447bb8b63
ocasioc3 <cjo9624@gmail.com>
2023-01-10 19:52:02 -0500

    Add 'mouseover' event-handling.

--- eas.js
+++ eas.js
@@ -1,13 +1,35 @@
 const container = document.getElementById('container');
+let grid_length = 16;
+
+function randomColor() {
+  let color = [];
+  for (let i = 0; i < 3; i++) {
+    color.push(Math.floor(Math.random() * 256));
+  }
+  return 'rgb(' + color.join(', ') + ')';
+}
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement('div');
-    cell.innerText = c + 1;
     container.appendChild(cell).className = 'grid-item';
+
+    cell.addEventListener('mouseenter', function () {
+      cell.style.backgroundColor = randomColor();
+    });
   }
 }
 
-makeRows(16, 16);
+function newGrid(rows, cols) {
+  grid_length = parseInt(
+    window.prompt('How many squares per side for the new grid?', '16')
+  );
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+  makeRows(grid_length, grid_length);
+}
+
+makeRows(grid_length, grid_length);
